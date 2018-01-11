746f8da4e2937bd9abc4d623a6a7838868c96486
boctavian96 <boctavian96@yahoo.com>
2018-01-11 23:17:12 +0200

    update

--- src/java/com/octavian/config/DbConfig.java
+++ src/java/com/octavian/config/DbConfig.java
@@ -11,4 +11,11 @@ package com.octavian.config;
  */
 public interface DbConfig {
     
+    public static final String DB_USER = "root";
+    public static final String DB_HOST = "localhost";
+    public static final String DB_PASS = "sys";
+    public static final String URL = "jdbc:mysql://localhost:3306/";
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String DATABASE = "BankDB";
+    
 }
--- src/java/com/octavian/database/DBManager.java
+++ src/java/com/octavian/database/DBManager.java
@@ -5,7 +5,13 @@
  */
 package com.octavian.database;
 
+import com.octavian.config.DbConfig;
+
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -13,6 +19,47 @@ import java.sql.Connection;
  */
 public class DBManager {
     
-    private Connection conn;
+    private Connection connection = null;
+    
+    //object Statement interface, used for executing sql statements
+    private Statement statement = null;
+    
+    //Resultset, uses a table for output data from the table
+    private ResultSet rs = null;
+    
+    //QueryStatus
+    private boolean queryStatus;
+    
+    public void connectToDB(){
+        try{
+            Class.forName(DbConfig.DRIVER);
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        try{
+            connection = DriverManager.getConnection(DbConfig.URL + DbConfig.DATABASE, DbConfig.DB_USER, DbConfig.DB_PASS);
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void executeQuery(String query){
+        
+        try{
+         statement = connection.createStatement();
+         
+            //Executam instructiunea Query
+         statement.execute(query);
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+        
+    public static void main(String[] args){
+        DBManager dbm = new DBManager();
+        
+        dbm.connectToDB();
+        dbm.executeQuery("SELECT * FROM Clients");
+    }
     
 }
